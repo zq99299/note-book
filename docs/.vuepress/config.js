ef4ca3f33d3ffb4e5e17cd92885c57f4fcf2b154
zhuqiang <zhuqiang@tidebuy.net>
2018-09-04 14:26:23 +0800

    更改主语言

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,13 @@ module.exports = {
     markdown: {
         lineNumbers: true
     },
+    locales: {
+        // 键名是该语言所属的子路径
+        // 作为特例，默认语言可以使用 '/' 作为其路径。
+        '/': {
+            lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+        }
+    },
     // theme: 'vue',
     themeConfig: {
         docsDir: 'docs',
