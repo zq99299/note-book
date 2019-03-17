dba3766ddbb2a29898f5b47dcdf20cda26f6d7ad
zq99299 <99299684@qq.com>
2019-03-17 22:33:57 +0800

    redis 开坑

--- docs/cache-pdp/index.js
+++ docs/cache-pdp/index.js
@@ -6,6 +6,29 @@ module.exports = () => {
       '003.md',
       '004.md',
       '005.md',
-      '006.md'
+      '006.md',
+      {
+        title: "redis",
+        collapsable: true,
+        children: [
+          'redis/007.md',
+          'redis/008.md',
+          'redis/009.md',
+          'redis/010.md',
+          'redis/011.md',
+          'redis/012.md',
+          'redis/013.md',
+          'redis/014.md',
+          'redis/015.md',
+          'redis/016.md',
+          'redis/017.md',
+          'redis/018.md',
+          'redis/019.md',
+          'redis/020.md',
+          'redis/021.md',
+          'redis/022.md',
+          'redis/023.md'
+        ]
+      },
     ]
 }
