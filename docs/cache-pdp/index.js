47210c7d9400f1f0e89ec541fa31410b8a7a69ba
zq99299 <99299684@qq.com>
2019-03-24 00:56:38 +0800

    024. 在项目中以经典的 3 节点方式部 和添加几章待学习坑位

--- docs/cache-pdp/index.js
+++ docs/cache-pdp/index.js
@@ -1,34 +1,30 @@
 module.exports = () => {
     return [
-      '',
-      '001-introduce.md',
-      '002.md',
-      '003.md',
-      '004.md',
-      '005.md',
-      '006.md',
-      {
-        title: "redis",
-        collapsable: true,
-        children: [
-          'redis/007.md',
-          'redis/008.md',
-          'redis/009.md',
-          'redis/010.md',
-          'redis/011.md',
-          'redis/012.md',
-          'redis/013.md',
-          'redis/014.md',
-          'redis/015.md',
-          'redis/016.md',
-          'redis/017.md',
-          'redis/018.md',
-          'redis/019.md',
-          'redis/020.md',
-          'redis/021.md',
-          'redis/022.md',
-          'redis/023.md'
-        ]
-      },
+        '',
+        '001-introduce.md',
+        '002.md',
+        '003.md',
+        '004.md',
+        '005.md',
+        '006.md',
+        {
+            title: "redis",
+            collapsable: true,
+            children: buildSc(7, 34, 'redis/')
+        },
     ]
+}
+
+function buildSc(start, end, parentDir) {
+    let mds = []
+    for (let i = start; i <= end; i++) {
+        if (i < 10) {
+            mds.push(`${parentDir}00${i}.md`)
+        } else if (i < 100) {
+            mds.push(`${parentDir}0${i}.md`)
+        } else {
+            mds.push(`${parentDir}${i}.md`)
+        }
+    }
+    return mds
 }
