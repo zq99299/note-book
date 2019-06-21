ce567d1d5cf14b69deb7cd76d8f927ad36c6dc26
zq99299 <99299684@qq.com>
2019-06-21 16:39:46 +0800

    服务端输出格式支持
    嵌入配置服务器 / Embedding the Config Server

--- docs/spring-cloud-tutorial/config/index.js
+++ docs/spring-cloud-tutorial/config/index.js
@@ -6,7 +6,10 @@ module.exports = (path) => {
       children: [
         path + '',
         path + '001.md',
-        path + '002.md'
+        path + '002.md',
+        path + '003.md',
+        path + '004.md',
+        path + '005.md'
       ]
     }
   ]
