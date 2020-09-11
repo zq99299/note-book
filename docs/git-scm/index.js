8e3ec955be4e68b3ff7b6c323714cf9477b76690
zq99299 <99299684@qq.com>
2020-09-11 17:47:01 +0800

    分支简介 分支的新建与合并 分支管理 分支开发工作流 远程分支

--- docs/git-scm/index.js
+++ docs/git-scm/index.js
@@ -1,6 +1,7 @@
 module.exports = () => {
   return [
     '',
+    '01/',
     {
       title: 'Git 基础',
       collapsable: false,
@@ -13,6 +14,19 @@ module.exports = () => {
         '02/05.md',
         '02/06.md',
         '02/07.md'
+      ]
+    },
+    {
+      title: '分支',
+      collapsable: false,
+      children: [
+        '03/',
+        '03/01.md',
+        '03/02.md',
+        '03/03.md',
+        '03/04.md',
+        '03/05.md',
+        '03/06.md'
       ]
     }
   ]
