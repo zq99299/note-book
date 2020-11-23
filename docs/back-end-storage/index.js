3b0eea7d8a7b2d95f75f536254c511ceddadacee
zq99299 <99299684@qq.com>
2020-11-23 15:45:26 +0800

    高速增长篇目录编排

--- docs/back-end-storage/index.js
+++ docs/back-end-storage/index.js
@@ -4,9 +4,9 @@ module.exports = () => {
     '00/',
     {
       title: '创业篇',
-      collapsable: false,
+      collapsable: true,
       children: [
-        '',
+        '01/',
         '01/01.md',
         '01/02.md',
         '01/03.md',
@@ -14,6 +14,20 @@ module.exports = () => {
         '01/05.md',
         '01/06.md',
         '01/07.md'
+      ]
+    },
+    {
+      title: '高速增长篇',
+      collapsable: true,
+      children: [
+        '02/',
+        '02/01.md',
+        '02/02.md',
+        '02/03.md',
+        '02/04.md',
+        '02/05.md',
+        '02/06.md',
+        '02/07.md'
       ]
     }
   ]
