a219f2788045103e53c321f649aea7d069fd1d3f
zq99299 <99299684@qq.com>
2020-12-29 19:40:52 +0800

    正则入门课开坑

---
+++ docs/regular/index.js
@@ -0,0 +1,42 @@
+module.exports = () => {
+  return [
+    '',
+    {
+      title: '01 | 开篇',
+      collapsable: true,
+      children: [
+        '01/',
+        '01/01.md',
+        '01/02.md'
+      ]
+    },
+    {
+      title: '02 | 基础篇',
+      collapsable: true,
+      children: [
+        '02/'
+      ]
+    },
+    {
+      title: '03 | 应用篇',
+      collapsable: true,
+      children: [
+        '03/'
+      ]
+    },
+    {
+      title: '04 | 加餐',
+      collapsable: true,
+      children: [
+        '04/'
+      ]
+    },
+    {
+      title: '05 | 结束',
+      collapsable: true,
+      children: [
+        '05/'
+      ]
+    }
+  ]
+}
