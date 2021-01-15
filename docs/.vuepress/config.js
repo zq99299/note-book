418cdfdfc07d64f4653e12d1ef9d9463eead0fb6
zq99299 <99299684@qq.com>
2021-01-15 10:15:52 +0800

    更名

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,7 @@ const oath2Sc = require('../oath2')
 const regularSc = require('../regular')
 
 module.exports = {
-  title: 'Mrcode 笔记本',
+  title: 'MRCODE-BOOK',
   description: '用来记录工作和学习过程中的笔记，汇总成册方便查阅，类容涵盖各类技术，如：Java、Git、ElasticSearch、MyCat、设计模式、Gradle、Vue - mrcode.cn',
   base: '/note-book/', // gh-page 中是增加了项目名
   dest: 'build/.vuepress/dist',  // 目录配置在外,纯粹是有代码洁癖和强迫症，并不能规避开发模式下同时构建不报错的问题
